0f7cb2a5c2cb039a2e9681f9b46528732da7a991
fancisleal <francis.leal@outlook.com>
2019-12-03 17:49:09 -0300

    iteracao com usuario

--- app/src/main/java/com/minhaopiniao/opiniaodetudo/MainActivity.kt
+++ app/src/main/java/com/minhaopiniao/opiniaodetudo/MainActivity.kt
@@ -2,11 +2,24 @@ package com.minhaopiniao.opiniaodetudo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonSave = findViewById<Button>(R.id.button_save)
+        val textViewName = findViewById<TextView>(R.id.input_nome)
+        val textViewReview = findViewById<TextView>(R.id.input_review)
+
+        buttonSave.setOnClickListener {
+            val name = textViewName.text
+            val review = textViewReview.text
+            Toast.makeText(this, "Nome:$name - Opinião:$review", Toast.LENGTH_LONG).show();
+        }
     }
 }
