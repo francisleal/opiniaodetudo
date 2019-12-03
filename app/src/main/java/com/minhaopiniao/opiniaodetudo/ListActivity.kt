84b8efcdb817a66edd7bf545b371a350fb9b4d89
fancisleal <francis.leal@outlook.com>
2019-12-03 20:33:16 -0300

    Listagem

---
+++ app/src/main/java/com/minhaopiniao/opiniaodetudo/ListActivity.kt
@@ -0,0 +1,26 @@
+package com.minhaopiniao.opiniaodetudo
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import com.minhaopiniao.opiniaodetudo.model.repository.ReviewRepository
+
+class ListActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.list_review_layout)
+
+        val intent = Intent(this, ListActivity::class.java)
+
+
+        val listView = findViewById<ListView>(R.id.list_recyclerview)
+        val reviews = ReviewRepository.instance.listAll()
+        val stringList = reviews.map { "${it.name} - ${it.review}" }
+
+        val adapter = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, stringList );
+
+        listView.adapter = adapter
+    }
+}
